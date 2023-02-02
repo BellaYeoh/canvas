bbff32a13529c584e02c35942c76e9e89ad0428b
bella yeoh <yy1336316@126.com>
2023-02-02 16:53:54 +0800

    feat:add a lot of mountain effect

--- mountainLikeCloud/mountain.js
+++ mountainLikeCloud/mountain.js
@@ -12,7 +12,16 @@ const startY = clientHeight;
 mountainSvg.setAttribute('width', clientWidth);
 mountainSvg.setAttribute('height', clientHeight);
 
-mountainSvg.appendChild(drawPolygon(startX, startY));
+for (var i = 0; i < 12; i++) {
+  mountainSvg.appendChild(
+    drawPolygon(
+      i % 2
+        ? startX + Math.floor(Math.random() * 10) * 100
+        : startX - Math.floor(Math.random() * 10) * 100,
+      startY
+    )
+  );
+}
 
 document.body.appendChild(mountainSvg);
 
@@ -25,19 +34,26 @@ function drawPolygon(startX, startY) {
   let points = '';
 
   for (var i = 0; i < 6; i++) {
-    points += `${startX + i * 100} ${startY - i * 20},`;
+    points += `${startX + i * 100} ${
+      startY - (Math.random() + i) * 20
+    },`;
   }
 
-  for (var i = 5; i >= 0; i--) {
+  for (var i = 4; i >= 0; i--) {
     if (i === 0) {
-      points += `${startX + (10 - i) * 100} ${startY - i * 20}`;
+      points += `${startX + (10 - i) * 100} ${
+        startY - (Math.random() + i) * 20
+      }`;
     } else {
-      points += `${startX + (10 - i) * 100} ${startY - i * 20},`;
+      points += `${startX + (10 - i) * 100} ${
+        startY - (Math.random() + i) * 20
+      },`;
     }
   }
 
   polygon.setAttribute('points', points);
   polygon.setAttribute('fill', '#2f90b9');
+  polygon.setAttribute('stroke', '#dddddd');
 
   polygon.appendChild(createAnimation());
 
@@ -53,6 +69,6 @@ function createAnimation() {
   animation.setAttribute('path', `M 0 0 L ${clientWidth} 0`);
 
   animation.setAttribute('repeatCount', 'indefinite');
-  animation.setAttribute('dur', '20s');
+  animation.setAttribute('dur', '15s');
   return animation;
 }
